c0ea798ca30d3c39267e40b5c4ca330793f1aec2
BagasWihant <bagaswihant@gmail.com>
2024-01-05 11:22:55 +0700

    modify view & message upload

--- resources/views/parse-text0100.blade.php
+++ resources/views/parse-text0100.blade.php
@@ -81,67 +81,7 @@
     </div>
 @endsection
 
+
 @push('js')
-    <script>
-        var previewNode = document.querySelector("#template");
-        previewNode.id = "";
-        var previewTemplate = previewNode.parentNode.innerHTML;
-        previewNode.parentNode.removeChild(previewNode);
-
-        var myDropzone = new Dropzone(".dropzone", {
-            maxFilesize: 1,
-            uploadMultiple: true,
-            autoProcessQueue: false,
-            parallelUploads: 100,
-            maxFiles: 20,
-            acceptedFiles: ".txt",
-            previewTemplate: previewTemplate,
-            previewsContainer: '#previews',
-            dictDefaultMessage: "<b>Ketuk atau seret file disini<b>",
-            removedfile: function(file) {
-                this.files.length < 1 && $('#clearFile').hide()
-                var fileName = file.name;
-
-                var _ref;
-                return (_ref = file.previewElement) != null ? _ref.parentNode.removeChild(file.previewElement) :
-                    void 0;
-            },
-            init: function() {
-                this.on("success", function(file, res) {
-                    $('#fileName').val(res)
-
-                    myDropzone.removeFile(file);
-                    $('#fileUpload').hide();
-                    $('#clearFile').hide()
-                    $('#backButton').hide();
-                    $('#downloadFile').show();
-                });
-
-                this.on('addedfile', function(file) {
-                    $('#clearFile').show()
-                })
-            }
-        });
-
-        $('#submitForm').click(function(e) {
-            e.preventDefault();
-            e.stopPropagation();
-            if (myDropzone.files.length < 1) return $.notify("Upload file dengan benar dulu ya gaiss..", {
-                className: "info"
-            });
-            myDropzone.processQueue();
-        });
-        $('#downloadButton').click(function(e) {
-            myDropzone.removeAllFiles();
-            $('#fileUpload').show();
-            $('#downloadFile').hide();
-            $('#backButton').show();
-        });
-        function clearFile(){
-            files = myDropzone.files
-            files.map(function(file){
-                myDropzone.removeFile(file)
-            })
-        }
-    </script>
+<x-jsCode />
 @endpush
